5c68e1d80094f26869aed9c686ed6939f1060a97
Stacy Acker <stacyacker@Stacys-MacBook-Pro.local>
2019-03-11 18:56:08 -0400

    added HEroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = procees.env.PORT ||3000;
+var PORT = process.env.PORT ||3000;
 
 // Initialize Express
 var app = express();
@@ -27,7 +27,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+var mongoURI=process.env.MONGODB_URI||"mongodb://localhost/unit18Populater";
+mongoose.connect(mongoURI, { useNewUrlParser: true });
 
 // Routes
 
